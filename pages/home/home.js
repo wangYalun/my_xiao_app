f1235cb6ff03feccf95c1000d32539feb6aadac4
wangYalun <326402399@qq.com>
2017-04-06 18:02:09 +0800

    图片查看器&评论点赞

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,22 +4,37 @@ var app = getApp();
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
+    userInfo: {},
+    swiper: {
+      imgUrls: [
+        'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+        'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+        'http://imgsrc.baidu.com/baike/pic/item/d01373f082025aaff92dd92bfaedab64034f1a36.jpg'
+      ],
+      indicatorDots: false,
+      autoplay: false,
+      interval: 5000,
+      duration: 1000
+    }
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: 'user'
     })
+  },
+  tapPanel:function(){
+    console.log("点击了评论");
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
